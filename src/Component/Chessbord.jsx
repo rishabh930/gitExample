c72da9edaf31a8288030c9e212753103697efcf4
[rishabh930] <[rishabhsharma467@gmail.com]>
2022-12-08 12:26:05 +0530

    Chessbord

---
+++ src/Component/Chessbord.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './Chessbord.css';
+const Chessbord = () => {
+  return (
+    <div className="ChessBord-main">
+      <div className="first-line">
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+      </div>
+      <div className="first-line">
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+        <div className="balck-block"></div>
+        <div className="white-block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Chessbord;
