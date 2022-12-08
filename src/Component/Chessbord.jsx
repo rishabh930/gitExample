4c02d8250b76ceb1718373626cbc4b706c8878be
[rishabh930] <[rishabhsharma467@gmail.com]>
2022-12-08 14:08:36 +0530

    main branch

--- src/Component/Chessbord.jsx
+++ src/Component/Chessbord.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Chessbord.css';
+import Pown from './kati/pown';
 const Chessbord = () => {
   return (
     <div className="ChessBord-main">
       <div className="first-line">
-        <div className="white-block"></div>
+        <div className="white-block">
+          <Pown  />
+        </div>
         <div className="balck-block"></div>
         <div className="white-block"></div>
         <div className="balck-block"></div>
