c0d5293c4499702ae88743a22807b6ccfc21a3e8
[rishabh930] <[rishabhsharma467@gmail.com]>
2022-12-09 17:22:18 +0530

    some change

---
+++ src/Eaxmple/pawn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { blackKing } from '../Component/kati/Blackpice';
+
+const Pawn = () => {
+
+
+
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div>
+        <img src="" />
+      </div>
+    </div>
+  );
+};
+
+export default Pawn;
